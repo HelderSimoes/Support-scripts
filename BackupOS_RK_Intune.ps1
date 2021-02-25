4e28f01e087c149e052c363b96920f18c60c9d0e
HelderSimoes <79648455+HelderSimoes@users.noreply.github.com>
2021-02-25 14:22:32 +0000

    Add files via upload

---
+++ BackupOS_RK_Intune.ps1
@@ -0,0 +1,8 @@
+﻿$osbde = Get-BitLockerVolume | where { ($_.volumetype -eq 'OperatingSystem') -and ($_.protectionstatus -eq 'On')}
+$bdeosprot = $osbde.KeyProtector
+foreach ( $prot in $bdeosprot) {
+    if ( $prot.KeyProtectorType -eq 'RecoveryPassword') {
+        $bdeosrkID = $prot.keyprotectorId
+        BackupToAAD-BitLockerKeyProtector -MountPoint $osbde.MountPoint -KeyProtectorId $bdeosrkID
+           }
+    }
